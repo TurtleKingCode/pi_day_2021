1458f6f3b3d6537fe5a4a85ef20903293847220a
Franklin Oguama <turtle.foguama@gmail.com>
2021-03-16 01:48:19 +0000

    Completed

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 var font;
 var vehicles = [];
+var pointLocation = [];
 
-var texts = [];
 var nextT = 0;
 var maxChangeForce = 20;
 
-var instructions = [];
-var insText = 'Click anywhere to change the Text';
+// var instructions = [];
+// var insText = 'Click anywhere to change the Text';
 
 function preload() {
 	font = loadFont('AkayaTelivigala-Regular.ttf');
@@ -14,57 +14,53 @@ function preload() {
 
 function setup() {
 	createCanvas(1000, 500);
-	background(51);
 
-	var bounds = font.textBounds(texts[nextT], 0, 0, 192);
-	var posx = width / 2 - bounds.w / 2;
-	var posy = height / 2 + bounds.h / 2;
+	textAlign(CENTER, CENTER);
+	textFont(font);
 
-	var points = font.textToPoints(texts[nextT], posx, posy, 192, {
-		sampleFactor: 0.07
-	});
+
+	var points = gridPoints();
 
 	for (var i = 0; i < points.length; i++) {
 		var pt = points[i];
-		var vehicle = new Vehicle(pt.x, pt.y);
+		var vehicle = new Vehicle(pt.x, pt.y, piDigit(i, points.length, 0), 8);
 		vehicles.push(vehicle);
 	}
 
-	var boundsIns = font.textBounds(insText, 0, 0, 30);
-	var posxIns = width / 2 - boundsIns.w / 2;
-	var posyIns = height / 6 + boundsIns.h / 2;
+	// var boundsIns = font.textBounds(insText, 0, 0, 30);
+	// var posxIns = width / 2 - boundsIns.w / 2;
+	// var posyIns = height / 6 + boundsIns.h / 2;
 
-	var insAr = split(insText, ' ');
+	// var insAr = split(insText, ' ');
 
-	for (var i = 0; i < insAr.length; i++) {
-		var bounds2 = font.textBounds(insAr[i], 0, 0, 30);
-		var posx2 = posxIns;
-		var posy2 = posyIns;
+	// for (var i = 0; i < insAr.length; i++) {
+	// var bounds2 = font.textBounds(insAr[i], 0, 0, 30);
+	// var posx2 = posxIns;
+	// var posy2 = posyIns;
 
-		posxIns += bounds2.w + 10;
+	// posxIns += bounds2.w + 10;
 
-		var points2 = font.textToPoints(insAr[i], posx2, posy2, 30, {
-			sampleFactor: 0.3
-		});
+	// var points2 = font.textToPoints(insAr[i], posx2, posy2, 30, {
+	// sampleFactor: 0.3
+	// });
 
-		for (var j = 0; j < points2.length; j++) {
-			var pt = points2[j];
-			var v = new Vehicle(pt.x, pt.y, 3);
-			instructions.push(v);
-		}
-	}
+	// for (var j = 0; j < points2.length; j++) {
+	// 	var pt = points2[j];
+	// 	var v = new Vehicle(pt.x, pt.y, 3);
+	// 	instructions.push(v);
+	// }
+	// }
 }
 
 function draw() {
-	console.log('b')
 	background(51);
 
-	for (var i = 0; i < instructions.length; i++) {
-		var v = instructions[i];
-		v.behaviors();
-		v.update();
-		v.show();
-	}
+	// for (var i = 0; i < instructions.length; i++) {
+	// 	var v = instructions[i];
+	// 	v.behaviors();
+	// 	v.update();
+	// 	v.show();
+	// }
 
 	for (var i = 0; i < vehicles.length; i++) {
 		var v = vehicles[i];
@@ -75,16 +71,20 @@ function draw() {
 }
 
 function mouseClicked() {
-	nextT++;
-	nextT %= text.length;
+	// nextT++;
 
-	var bounds = font.textBounds(texts[nextT], 0, 0, 192);
-	var posx = width / 2 - bounds.w / 2;
-	var posy = height / 2 + bounds.h / 2;
+	// nextT %= text.length;
+	gridSetup = !gridSetup;
 
-	var points = font.textToPoints(texts[nextT], posx, posy, 192, {
-		sampleFactor: 0.1
-	});
+	// var points = font.textToPoints(texts[nextT], posx, posy, 192, {
+	// sampleFactor: 0.1
+	// });
+
+	var points = gridSetup ? gridPoints() : piPoints();
+
+	var font_size = gridSetup ? 8 : 4;
+
+	var movedSpaces = gridSetup ? 0 : 65;
 
 	if (points.length < vehicles.length) {
 		// If new word requires less points than previous
@@ -94,6 +94,9 @@ function mouseClicked() {
 		for (var i = 0; i < points.length; i++) {
 			vehicles[i].target.x = points[i].x;
 			vehicles[i].target.y = points[i].y;
+			vehicles[i].txt = piDigit(i, points.length, movedSpaces);
+			vehicles[i].r = font_size;
+			vehicles[i].hue = points.length - i;
 			// Reassing the targets of every point
 			var force = p5.Vector.random2D();
 			force.mult(random(maxChangeForce));
@@ -111,6 +114,9 @@ function mouseClicked() {
 		for (var i = 0; i < points.length; i++) {
 			vehicles[i].target.x = points[i].x;
 			vehicles[i].target.y = points[i].y;
+			vehicles[i].txt = piDigit(i, points.length, movedSpaces);
+			vehicles[i].r = font_size;
+			vehicles[i].hue = i;
 			// Reassign the targets for every point
 			var force = p5.Vector.random2D();
 			force.mult(random(maxChangeForce));
@@ -123,6 +129,9 @@ function mouseClicked() {
 		for (var i = 0; i < points.length; i++) {
 			vehicles[i].target.x = points[i].x;
 			vehicles[i].target.y = points[i].y;
+			vehicles[i].txt = piDigit(i, points.length, movedSpaces);
+			vehicles[i].r = font_size;
+			vehicles[i].hue = i;
 
 			var force = p5.Vector.random2D();
 			force.mult(random(maxChangeForce));
